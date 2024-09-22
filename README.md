9bf4cec07d3c2eed5c2360c7ab23ab1057bf3baa
telicoscience <telico@yahoo.com.br>
2024-09-22 14:56:59 -0300

    Upload do arquivo principal

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# shell_backup
- Cria um backkup de todos os arquivos em shell que estiver no diretório atual 
+# copy_shell_scripts.sh
+
+## Descrição
+
+O script `copy_shell_scripts.sh` busca todos os arquivos com extensão `.sh` (arquivos Shell Script) no diretório atual (onde o script é executado) e os copia para um diretório chamado `scripts_shell`. O diretório de destino deve já estar criado no local de execução do script.
+
+## Uso
+
+Para usar este script, siga os passos abaixo:
+
+1. Garanta que você tenha o diretório `scripts_shell` criado no mesmo diretório onde o script será executado.
+   
+   Se o diretório `scripts_shell` não existir, você pode criá-lo usando o seguinte comando:
+   ```bash
+   mkdir scripts_shell
---
+++ copy_shell_script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# --------------------------------------------------------
+# Script: copy_shell_scripts.sh
+# Descrição: Este script busca todos os arquivos .sh no diretório
+#            atual (onde o script é executado) e os copia para um
+#            diretório de destino chamado "scripts_shell". 
+#
+# Uso: ./copy_shell_scripts.sh
+#
+# Requisitos: O diretório "scripts_shell" deve existir no diretório
+#             atual. O script verifica a existência dessa pasta e
+#             exibe uma mensagem de erro caso ela não esteja presente.
+#
+# Autor: [Seu Nome]
+# Licença: GPL-3.0
+# --------------------------------------------------------
+
+# Definir a pasta de destino
+dest_dir="scripts_shell"
+
+# Verifica se a pasta de destino existe 
+if [ ! -d "$dest_dir" ]; then 
+    echo "A pasta $dest_dir não existe. Por favor, crie a pasta para continuar."
+    exit 1
+fi
+
+# Encontra todos os arquivos .sh no diretório atual e os copia para a pasta script_shell
+find . -type f -name "*.sh" -exec cp {} "$dest_dir" \;
+
+echo "Cópia de arquivos .sh concluída!"
